6dc8fcd942c3c4484d33c9fb65c5d2e23287ec3b
rokkomatar <rok.komatar@gmail.com>
2018-08-10 14:38:38 +0200

    Lint fixes, still have to fix one of the test cases

---
+++ src/__tests__/Parser.test.ts
@@ -0,0 +1,78 @@
+const CSVParser = require('../../lib/index');
+
+describe('CSVParser', function() {
+  describe('parseCSV()', function() {
+    it('should return empty array if empty file', () => {
+      let parser = new CSVParser('src/__tests__/emptyFile.csv');
+      return parser.parseCSV().then(data => {
+        expect(data).toHaveLength(0);
+      });
+    });
+
+    it('should return array of objects if headers allowed', function() {
+      let parser = new CSVParser('src/__tests__/data1.csv', { delimiter: ',', headers: true });
+      setTimeout(() => {}, 1000);
+      let expected = [
+        { 'full name': 'John Doe 1', ' AGE': ' 24', ' STATUS': ' ACTIVE' },
+        { 'full name': 'John Doe 2', ' AGE': ' 22', ' STATUS': ' BLOCKED' },
+        { 'full name': 'John Doe 3', ' AGE': ' 25', ' STATUS': ' ACTIVE' },
+      ];
+      return parser.parseCSV().then(data => {
+        expect(data).toEqual(expected);
+      });
+    });
+
+    it('should return array of arrays if headers disabled', function() {
+      let parser = new CSVParser('src/__tests__/data1.csv', { delimiter: ',', headers: false });
+      setTimeout(() => {}, 1000);
+      let expected = [
+        ['full name', ' AGE', ' STATUS'],
+        ['John Doe 1', ' 24', ' ACTIVE'],
+        ['John Doe 2', ' 22', ' BLOCKED'],
+        ['John Doe 3', ' 25', ' ACTIVE'],
+      ];
+      return parser.parseCSV().then(data => {
+        expect(data).toEqual(expected);
+      });
+    });
+
+    it('should handle different delimiters as ;', function() {
+      let parser = new CSVParser('src/__tests__/data2.csv', { delimiter: ';', headers: true });
+      setTimeout(() => {}, 1000);
+      let expected = [
+        { 'full name': 'John Doe 1', ' AGE': ' 24', ' STATUS': ' ACTIVE' },
+        { 'full name': 'John Doe 2', ' AGE': ' 22', ' STATUS': ' BLOCKED' },
+        { 'full name': 'John Doe 3', ' AGE': ' 25', ' STATUS': ' ACTIVE' },
+      ];
+      return parser.parseCSV().then(data => {
+        expect(data).toEqual(expected);
+      });
+    });
+
+    it('should handle different delimiters as \\t', function() {
+      let parser = new CSVParser('src/__tests__/data3.csv', { delimiter: '\t', headers: true });
+      setTimeout(() => {}, 1000);
+      let expected = [
+        { 'full name': 'John Doe 1', ' AGE': ' 24', ' STATUS': ' ACTIVE' },
+        { 'full name': 'John Doe 2', ' AGE': ' 22', ' STATUS': ' BLOCKED' },
+        { 'full name': 'John Doe 3', ' AGE': ' 25', ' STATUS': ' ACTIVE' },
+      ];
+      return parser.parseCSV().then(data => {
+        expect(data).toEqual(expected);
+      });
+    });
+
+    it('should handle different delimiters as |', function() {
+      let parser = new CSVParser('src/__tests__/data4.csv', { delimiter: '|', headers: true });
+      setTimeout(() => {}, 1000);
+      let expected = [
+        { 'full name': 'John Doe 1', ' AGE': ' 24', ' STATUS': ' ACTIVE' },
+        { 'full name': 'John Doe 2', ' AGE': ' 22', ' STATUS': ' BLOCKED' },
+        { 'full name': 'John Doe 3', ' AGE': ' 25', ' STATUS': ' ACTIVE' },
+      ];
+      return parser.parseCSV().then(data => {
+        expect(data).toEqual(expected);
+      });
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,77 @@
-
 // ==========================
 // Created by: Rok Komatar
 // ==========================
 
 // Require file stream api to read csv files
-let fs = require('fs');
 
-class CSVParser {
-    parsedData: any;
-	// Initialize dilimeter and headers option in constructor
+import fs = require('fs');
 
-	constructor(public csvFile: string, public options : {delimiter: "-", headers: false}) {
-		this.csvFile = csvFile;
-		this.options = options;
-		this.parsedData = [];
+export class CSVParser {
+  protected parsedData: any;
+  // Initialize dilimeter and headers option in constructor
+
+  constructor(public csvFile: string, public options: { delimiter: '-'; headers: false }) {
+    this.csvFile = csvFile;
+    this.options = options;
+    this.parsedData = [];
+  }
+
+  // Read and parse the csv file and return final data.
+  protected parseCSV() {
+    // Return a new promise to make sure all the file is loaded
+    return new Promise((resolve: any, reject: any) => {
+      fs.readFile(this.csvFile, (err: any, fileData: any) => {
+        // Throws error if not exists
+        if (err) {
+          throw err;
+        }
+        // Separate all lines
+        const data = fileData.toString().split('\n');
+
+        // Check if headers available generate array of objects else generate array of arrays
+        if (this.options.headers) {
+          // Get headers from first row
+          const headers: string[] = data[0].trim().split(this.options.delimiter);
+          // Remove headers from the data
+          data.shift();
+          // Create json
+          this.parsedData = this.createJson(headers, this.createArray(data));
+        } else {
+          // Create an array of arrays
+          this.parsedData = this.createArray(data);
+        }
+        resolve(this.parsedData);
+      });
+    });
+  }
+
+  // Create an a rray of arrays
+  protected createArray(data: any) {
+    // Split all lines by the delimiter and create an array with row values
+
+    const finalData: string[] = [];
+    for (const row of data) {
+      finalData.push(row.trim().split(this.options.delimiter));
     }
- 
-	// Read and parse the csv file and return final data.
-	parseCSV() {
-        // Return a new promise to make sure all the file is loaded
-        
-		return new Promise((resolve: any, reject: any) => {
-		    fs.readFile(this.csvFile, (err: any, fileData: any) => {
-		    	// Throws error if not exists
-		        if (err) {
-		        	console.error(err);
-		            throw err;
-		        }
-		        // Separate all lines
-				let data = fileData.toString().split('\n');
-				
-				if(data.length === 1)
-					throw this.parsedData;
 
-		        // Check if headers available generate array of objects else generate array of arrays
-		        if (this.options.headers) {
-					
-					// Get headers from first row
-					
-					
-					let headers:string[] = data[0].trim().split(this.options.delimiter);
-					
-					
-		        	data.shift();
-		        	// Create json 
-		        	this.parsedData = this.createJson(headers, this.createArray(data));
-		        } else {
-		        	// Create an array of arrays
-		        	this.parsedData = this.createArray(data);
-		        }
-		        resolve(this.parsedData);
-		    });		
-		})
-	}
+    return finalData;
+  }
 
-	// Create an a rray of arrays
-	createArray(data: any) {
-		// Split all lines by the delimiter and create an array with row values
-		for (let i = 0; i < data.length; i++) {
-			data[i] = data[i].trim().split(this.options.delimiter);
-		}
-		return data;
-	}
+  // Generate array of objects, using the first row as keys
 
-    // Generate array of objects, using the first row as keys
-    
-	createJson(headers: any, data: any) {
-		let finalJson = [];
-		
-		for (let i:number = 0; i < data.length; i++) {
-			let obj:any = {};
-			
-			for (let j:number= 0; j < data[i].length;j++) {
-				obj[headers[j]] = data[i][j];
-			}
-			finalJson.push(obj);
-		}
-		
-		return finalJson;
+  protected createJson(headers: string[], data: any) {
+    const finalJson: any = [];
+
+    for (const [i, value] of data.entries()) {
+      const obj: any = {};
+
+      for (const [j] of value.entries()) {
+        obj[headers[j]] = data[i][j];
+      }
+      finalJson.push(obj);
     }
-    
+    return finalJson;
+  }
 }
 
 module.exports = CSVParser;
